9c7650887db6c8cd83b3367563e8ce5bb9935bbd
n-gottn001 <nikitha.gottam@capgemini.com>
2016-07-27 15:33:46 -0500

    adding a comment to test CI 1

--- src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicServiceImpl.java
@@ -58,7 +58,7 @@ public class ClinicServiceImpl implements ClinicService {
     @Override
     @Transactional(readOnly = true)
     public Collection<PetType> findPetTypes() throws DataAccessException {
-		System.out.println("This is a comment");
+		System.out.println("This is a comment to test CI");
         return petRepository.findPetTypes();
     }
 
